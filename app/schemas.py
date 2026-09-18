7bef7232c7ee43cf3f31bed771c664b2f7bf1150
catcom9 <149201430+catcom9@users.noreply.github.com>
2026-09-18 10:42:20 +0000

    Fixed typo

--- app/schemas.py
+++ app/schemas.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, EmailStr, Field, StringConstraints
 
 class UserCreate(BaseModel):
     user_id: int = Field(gt=0)
-    name: Annotated[str, StringConstraints(min_lenght=2, max_lenght=50)]
+    name: Annotated[str, StringConstraints(min_length=2, max_length=50)]
     email: EmailStr
     age: int = Field(gt=1, lt=120)
     student_id: Annotated[str, StringConstraints(pattern=r"^S\d{7}$")]
