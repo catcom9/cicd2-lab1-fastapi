2cfebbfb73d2c99c146ed60ee40d272ec469dfc4
catcom9 <149201430+catcom9@users.noreply.github.com>
2026-09-18 10:40:23 +0000

    Added the add user endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+
+from app.schemas import UserCreate
 
 app = FastAPI(title="Lab 1 - FastAPI User API")
+
+users: list[UserCreate] = []
 
 @app.get("/health")
 def health():
@@ -9,3 +13,14 @@ def health():
 @app.get("/hello")
 def hello():
     return {"message": "Hello from FastAPI"}
+
+@app.post("/api/users", status_code=status.HTTP_201_CREATED)
+def add_user(new_user: UserCreate):
+    for existing_user in users:
+        if existing_user.user_id == new_user.user_id:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="A user with this user id already exists!"
+            )
+    users.append(new_user)
+    return new_user
